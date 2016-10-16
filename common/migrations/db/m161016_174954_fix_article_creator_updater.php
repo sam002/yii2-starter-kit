a3a001b46059d064799acffd7c1ae848f6ee6066
Semen Dubina <semen@sam002.net>
2016-10-16 21:35:59 +0300

    Revert migration and create new for articles

---
+++ common/migrations/db/m161016_174954_fix_article_creator_updater.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+class m161016_174954_fix_article_creator_updater extends Migration
+{
+    public function safeUp()
+    {
+        $tableOptions = null;
+
+        $this->dropForeignKey('fk_article_author', '{{%article}}');
+        $this->dropForeignKey('fk_article_updater', '{{%article}}');
+
+        $this->renameColumn('{{%article}}', 'author_id', 'created_by');
+        $this->renameColumn('{{%article}}', 'updater_id', 'updated_by');
+
+        $this->addForeignKey('fk_article_created', '{{%article}}', 'created_by', '{{%user}}', 'id', 'cascade', 'cascade');
+        $this->addForeignKey('fk_article_updated', '{{%article}}', 'updated_by', '{{%user}}', 'id', 'set null', 'cascade');
+        $this->createIndex('idx_article_slug', '{{%article}}', 'slug', true);
+        $this->createIndex('idx_article_category_slug', '{{%article_category}}', 'slug', true);
+
+    }
+
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_article_created', '{{%article}}');
+        $this->dropForeignKey('fk_article_updated', '{{%article}}');
+
+        $this->renameColumn('{{%article}}', 'created_by', 'author_id');
+        $this->renameColumn('{{%article}}', 'updated_by', 'updater_id');
+
+        $this->addForeignKey('fk_article_author', '{{%article}}', 'author_id', '{{%user}}', 'id', 'cascade', 'cascade');
+        $this->addForeignKey('fk_article_updater', '{{%article}}', 'updater_id', '{{%user}}', 'id', 'set null', 'cascade');
+        $this->dropIndex('idx_article_slug', '{{%article}}');
+        $this->dropIndex('idx_article_category_slug', '{{%article_category}}');
+    }
+}
