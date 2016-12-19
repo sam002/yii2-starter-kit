72ae34ff0de649ae9131e5d36c191a1f13dbeb3c
Катерина <katech91@gmail.com>
2016-12-19 17:07:10 +0300

    #606 fixed. Table name in migration changed

--- common/migrations/db/m161218_124548_insert_to_key_storage_item_table.php
+++ common/migrations/db/m161218_124548_insert_to_key_storage_item_table.php
@@ -6,13 +6,13 @@ class m161218_124548_insert_to_key_storage_item_table extends Migration
 {
     public function up()
     {
-        $this->insert('key_storage_item', [
+        $this->insert('{{%key_storage_item}}', [
             'key' => 'common.default-avatar',
             'value' => '{"path":"anonymous.jpg","name":"anonymous.jpg","base_url":"'.env('FRONTEND_URL').'\/img"}']);
     }
 
     public function down()
     {
-        $this->delete('key_storage_item', ['key' => 'common.default-avatar']);
+        $this->delete('{{%key_storage_item}}', ['key' => 'common.default-avatar']);
     }
 }
