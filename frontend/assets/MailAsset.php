7e1c450d28e927522e0441f1478bd3dbcb91827a
Катерина <katech91@gmail.com>
2017-01-08 23:06:03 +0300

    #611 view of e-mail adapted to site style

---
+++ frontend/assets/MailAsset.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace frontend\assets;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class MailAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@frontendUrl';
+
+//    public $sourcePath = '@frontend/mail/layouts';
+
+    public $css = [
+        'css/html.css',
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+//        'common\assets\MaterialDesign',
+//        'common\assets\Html5shiv',
+    ];
+}
--- frontend/mail/passwordResetToken.php
+++ frontend/mail/passwordResetToken.php
@@ -7,9 +7,20 @@ use yii\helpers\Html;
 
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['/user/sign-in/reset-password', 'token' => $token]);
 ?>
-
-Hello <?php echo Html::encode($user->username) ?>,
-
-Follow the link below to reset your password:
-
-<?php echo Html::a(Html::encode($resetLink), $resetLink) ?>
+<table>
+    <tr>
+        <td>
+            <h3 class="text">Hello <?php echo Html::encode($user->username) ?>,</h3>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <h3 class="text">Follow the link below to reset your password:</h3>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <h3 class="text"><?php echo Html::a(Html::encode($resetLink), $resetLink) ?></h3>
+        </td>
+    </tr>
+</table>
